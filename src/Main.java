5b7cc7e8f62592c58d473d60bc150191830e9211
Sfa11eN <sfallenn@gmail.com>
2024-01-23 19:04:38 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        String wort ="уверенинереву";
+        String wort ="уверен и не реву";
         char[] warray =wort.toCharArray();
         System.out.println(Arrays.toString(warray));
         System.out.println(Palindrom.istPalindrome(warray));
